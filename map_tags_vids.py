8233a2cca2ca6f73b0af1962f399de3a57609c67
Arvind <arvind.iisc@gmail.com>
2022-12-09 20:49:43 +0530

    maps

--- map_tags_vids.py
+++ map_tags_vids.py
@@ -1,18 +1,18 @@
+from collections import defaultdict
 import redis
 red6 = redis.Redis(host='localhost', port=6379, db=6,encoding='utf-8',decode_responses=True)
 
-result=[]
+result=defaultdict(list)
 def process(vid):
     tags=red6.smembers(vid)
     print(tags)
     tags=list(tags)
-    tags=tags[:3]
-    all_tags=",".join(tags)
-    item={}
-    item["vid"]=vid
-    item["tags"]=all_tags
-    result.append(item)
+    
+    
+    for tag in tags:
 
+        result[tag].append(vid)
+        
 
 
 
@@ -27,6 +27,6 @@ for line in lines[:500]:
 
 
 
-fp=open("out.json","w")
+fp=open("tv.json","w")
 json.dump(result,fp,indent=4)
 fp.close()
