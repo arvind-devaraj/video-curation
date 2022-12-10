6b171fa7e88f405f140c8e8be9e709ebfd031662
Arvind <arvind.iisc@gmail.com>
2022-12-10 13:52:28 +0530

    vids query

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -15,17 +15,12 @@ def index():
 @app.route('/test')
 def render_playlist():
 
-    #vids =get_vid_list(pl_id)
-    items=[{"vid":"ZVR2Way4nwQ","tags":["abc","def"]},
-          {"vid":"sgQAhG5Q7iY"},
-          {"vid":"YaKMeAlHgqQ"},
-          {"vid":"h0e2HAPTGF4"},
-          {"vid":"ofM8LE9Zeaw"} ]
+     
 
     fp = open("out.json")
     items=json.load(fp)
     print(items)
-    return render_template('videos.html',main_vid=items[0]["vid"], vid_list=items)
+    return render_template('videos.html',main_vid=items[0]["vid"], vid_list=items[:10])
 
 
 if __name__ == '__main__':
