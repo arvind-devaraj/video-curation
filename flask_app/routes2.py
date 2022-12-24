1e4ad73fbd16f6b3b76d4a5c4fae9dd690ad4037
Arvind <arvind.iisc@gmail.com>
2022-12-24 13:58:54 +0530

    fix

--- flask_app/routes2.py
+++ flask_app/routes2.py
@@ -16,12 +16,13 @@ def index():
 def render_playlist():
 
      
+    phrase = request.args.get("phrase")
 
     fp = open("../tv.json")
     items=json.load(fp)
     print(items)
-    return render_template('videos.html',main_vid=items["Deep Learning"][0], vid_list=items["Deep Learning"])
+    return render_template('videos.html',main_vid=items[phrase][0], vid_list=items[phrase])
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
